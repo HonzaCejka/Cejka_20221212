78a699e2cbf18018e5cb96342340fe0c40a356b8
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-12-12 12:22:11 +0100

    Add project files.

---
+++ Cejka_20221212/Soldier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cejka_20221212
+{
+    internal class Soldier
+    {
+        Random random = new Random();
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int MaxHP { get; set; }
+        public int DMG { get; set; }
+        public int Shield { get; set; }
+        public int MaxShield { get; set; }
+        public int Level { get; set; }
+        public Soldier(string name)
+        {
+            Name = name;
+            HP = 0;
+            MaxHP = 100;
+            DMG = 0;
+            Shield = 0;
+            MaxShield = 100;
+            Level = 1;
+        }
+        public void Attack(Soldier Enemy)
+        {
+
+        }
+        public void Heal()
+        {
+
+        }
+        public void LevelUp()
+        {
+
+        }
+    }
+}
