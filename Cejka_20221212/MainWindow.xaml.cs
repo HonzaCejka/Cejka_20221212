46c778eb1ea1cf7a2aa17a49694ea077f88b385a
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2022-12-12 13:25:26 +0100

    hotovo asi

--- Cejka_20221212/MainWindow.xaml.cs
+++ Cejka_20221212/MainWindow.xaml.cs
@@ -20,9 +20,90 @@ namespace Cejka_20221212
     /// </summary>
     public partial class MainWindow : Window
     {
+        Soldier soldier1;
+        Soldier soldier2;
+        int pick = 0;
         public MainWindow()
         {
             InitializeComponent();
+            soldier1 = new Soldier("soldier 1");
+            soldier2 = new Soldier("soldier 2");
+            picked(P1,P2);
+            Build(soldier1, txt1);
+            Build(soldier2, txt2);
         }
+        public void Build(Soldier sol,TextBlock txt)
+        {
+            txt.Text = " "+sol.Name;
+            txt.Text += "\n HP: " + sol.HP;
+            txt.Text += "\n Shield: " + sol.Shield;
+            txt.Text += "\n Level: " + sol.Level;       
+            
+        }
+
+        public void picked(Border brd,Border Enemybrd)
+        {
+            brd.BorderThickness = new Thickness(1);
+            Enemybrd.BorderThickness = new Thickness(0);
+        }
+        public void Switch()
+        {
+            if (pick == 0)
+            {
+                pick = 1;
+                picked(P2, P1);
+            }
+            else
+            {
+                pick = 0;
+                picked(P1, P2);
+            }
+            if (soldier1.lost == true)
+            {
+                MessageBox.Show("soldier 2 vyhral");
+                Application.Current.Shutdown();
+            }
+            else if (soldier2.lost == true)
+            {
+                MessageBox.Show("soldier 1 vyhral");
+                Application.Current.Shutdown();
+            }
+        }
+        private void Switch_Click(object sender, RoutedEventArgs e)
+        {
+            Switch();
+        }
+
+        private void Hit_Click(object sender, RoutedEventArgs e)
+        {           
+            if (pick == 0)
+            {
+                soldier1.Attack(soldier2);
+            }
+            else
+            {
+                soldier2.Attack(soldier1);
+            }
+            Build(soldier1, txt1);
+            Build(soldier2, txt2);
+            soldier1.Check(soldier2);
+            soldier2.Check(soldier1);
+            Switch();
+            
+        }
+
+        private void Heal(object sender, RoutedEventArgs e)
+        {
+            if (pick == 0)
+            {
+                soldier1.Heal();
+            }
+            else
+            {
+                soldier2.Heal();
+            }
+            Build(soldier1, txt1);
+            Build(soldier2, txt2);
+        }       
     }
 }
--- Cejka_20221212/Soldier.cs
+++ Cejka_20221212/Soldier.cs
@@ -3,40 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Cejka_20221212
 {
-    internal class Soldier
+    public class Soldier
     {
         Random random = new Random();
         public string Name { get; set; }
+        public int DMGinRow { get; set; }
         public int HP { get; set; }
         public int MaxHP { get; set; }
         public int DMG { get; set; }
         public int Shield { get; set; }
         public int MaxShield { get; set; }
         public int Level { get; set; }
+        public bool lost = false;
         public Soldier(string name)
         {
             Name = name;
-            HP = 0;
+            HP = 100;
             MaxHP = 100;
-            DMG = 0;
-            Shield = 0;
+            DMG = random.Next(10, 100);
+            Shield = 100;
             MaxShield = 100;
             Level = 1;
         }
-        public void Attack(Soldier Enemy)
+        public void Check(Soldier Enemy)
         {
-
+            if (HP <=0)
+            {                
+                Enemy.lost = true;                
+            }            
+        }
+        public void Attack(Soldier Enemy)
+        {            
+            if (Enemy.Shield > 0)
+            {
+                Enemy.Shield -= DMG;                
+            }
+            else
+            {                 
+                Enemy.Shield = 0;
+                Enemy.HP -= DMG;
+                DMGinRow += DMG;                
+            }
+            LevelUp();
         }
         public void Heal()
         {
-
+            HP = MaxHP;
         }
         public void LevelUp()
         {
-
+            if (DMGinRow >= 100)
+            {
+                DMGinRow = 0;                
+                MaxHP += 20;
+                MaxShield += 20;
+                Shield = MaxShield;
+                Heal();
+                Level++;                
+            }
         }
     }
 }
